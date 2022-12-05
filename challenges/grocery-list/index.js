330098adde16894f4077f698c306bde44f600ece
Rob Vanarsdall <rvanar@gmail.com>
2022-12-05 14:39:11 -0500

    completed challenge

---
+++ challenges/grocery-list/index.js
@@ -0,0 +1,8 @@
+let para = document.getElementById("para");
+let list = document.getElementById("list");
+let title = document.getElementById("title");
+para.style.color = "red";
+
+list.style.listStyleType = "none";
+
+title.innerText = "This is My Grocery List";
